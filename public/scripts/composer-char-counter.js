99bcbdf45f7ffd3a9680ca7d0de5292a6b34562c
Emanuel Neves <emanuelneves10@gmail.com>
2017-08-07 21:04:30 +0000

    added character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,11 @@
 $(function(){
-  console.log('ready!')
+  $('.new-tweet > form > textarea').on('keyup', function(){
+    const counter = $(this).closest('form').children('.counter');
+    counter.text(140-this.value.length);
+    if (this.value.length > 140){
+      counter.addClass('overflow');
+    } else {
+      counter.removeClass('overflow');
+    }
+  });
 });
